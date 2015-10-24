48ff1cdd4e0426ad429f782db972d0cf78db17b4
Evan McNeil <evanmcneil1999@gmail.com>
2015-10-24 14:53:24 -0700

    Changes to the stuff

--- src/pid.cpp
+++ src/pid.cpp
@@ -1,4 +1,7 @@
 //Template for what a pid would look like, basically copied from existing AVNavControl
+#include "pid.h"
+
+
 
 void reset_pid()
 {
@@ -7,6 +10,32 @@ void reset_pid()
 
 void init_pid()
 {
+	if(pPitch != NULL)
+	{
+		delete pPitch;
+		delete pHead;
+		delete pDepth;
+	}
+	pPitch = new PID();
+	pHead  = new PID();
+	pDepth = new PID();
+	
+	//pPitch->setGains(); //I need to find out where these values come from
+	//pHead->setGains();
+	//pDepth->setGains();
+
+	pPitch->setBounds(-30,30);
+	pHead->setBounds(-180,180);
+	pDepth->setBounds(-6,6);
+	
+	pPitch->setSetpoint(0.0f);
+	pHead->setSetpoint(desHead);  
+	pDepth->setSetpoint(desDepth);
+	
+	pPitch->setScale(100.0f/35);
+	pHead->setScale(1.0f/180);
+	pDepth->setScale(1.0f/3);
+	
 	
 }
 
@@ -20,7 +49,7 @@ void update_motors()
 	
 }
 
-void give_data()
+void update_data()
 {
 	
 }
--- src/pid.h
+++ src/pid.h
@@ -3,24 +3,29 @@
 
 #include "Kalman.h"
 #include "pid_library.h"
+#include "defs.h"
 
-//Include: motor interface, simulator
+//Also include: motor interface, simulator
 
 //Info about pitch heading depth acceleration etc
 
 //Instances of the pid class (from pid_library) for each p/h/d
+//Kalman filters for p/h
+PID *pPitch, *pHead, *pDepth;
+Kalman pitchK, headK
 
 //Volatile vars that the pid receives from EVA to control it
-extern volatile int desHead, desDepth, desPower;
+extern volatile int desHead, desDepth, desPower, desStrafe;
 extern volatile int depth;
 extern volatile bool isAlive;
 
 //Some kind of motor array
 
+
 void reset_pid();
 void init_pid();
 void do_pid();
 void update_motors();
-void give_data();
+void update_data();
 
 #endif
